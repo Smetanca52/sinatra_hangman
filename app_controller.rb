5a6c3bfcc3103e0525696ccada3897fd0d7996a0
Ivan Smetanca <smetanca52ivan@gmail.com>
2019-03-06 11:27:23 +0200

    Created views and functionality

--- app_controller.rb
+++ app_controller.rb
@@ -1,8 +1,32 @@
   require 'sinatra'
-  require 'sinatra/reloader'
-  require './hangman.rb'
+  require 'sinatra/reloader' if development?
+  require './model/hangman.rb'
+
+  enable :sessions
 
   get '/' do
-    game = Hangman.new.play
-    erb :index, :locals => { :game => game }
+    redirect to '/new'  if session["game"].nil?
+    redirect to '/won'  if session["game"].show_correct == session["game"].word
+    redirect to '/lost' if session["game"].misses.length >= 8
+    guess = session["game"].show_correct
+    erb :game, :locals => {:guess => guess }
+  end
+
+  get '/new' do
+    session["game"] = Hangman.new
+    redirect to '/'
+  end
+
+  get '/won' do
+    erb :winner
+  end
+
+  get '/lost' do
+    erb :loser
+  end
+
+  post '/guess' do
+    guess = params["guess"]
+    session["game"].guess(guess)
+    redirect to '/'
   end
--- model/hangman.rb
+++ model/hangman.rb
@@ -1,100 +1,39 @@
-require "yaml"
-require "set"
-
 class Hangman
+attr_reader :misses
+attr_reader :word
 
-  def initialize
-    @guillotine = "__\n |\n"
-    @parts   = [ " O", "\n-", "|", "-", "\n/",  " \\" ].reverse
-    @lost = false
-    @won = false
-    @secret_word = get_random_word
-    @correct_guesses = []
-    @wrong_guesses = []
-    welcome_page
+def initialize
+  @word = get_random_word
+  @misses = []
+  @correct_letters = []
+end
+
+def get_random_word
+  rand_word = File.readlines('5desk.txt').select  do |w|
+    w.chomp.length > 4 && w.chomp.length < 13
   end
+  rand_word.sample.chomp.downcase
+end
 
-  def welcome_page
-    puts "Welcome to Hangman Game!"
-    print 'Load game? (y/n): '
-    if gets.chomp.downcase == 'y'
-      load_game
-    else
-      play
-    end
+def guess(letter)
+  return false if letter.nil?
+  letter = letter.downcase
+  if letter.length == 1  && ((@misses+@correct_letters).nil? || !(@misses+@correct_letters).include?(letter))
+      @word.chars.include?(letter) ? @correct_letters << letter : @misses << letter
+      return true
   end
+  false
+end
 
-  def save_game
-    File.open("save.txt", 'w') { |file| file.write(YAML::dump(self))}
-    puts "Game Saved!"
-    exit
-  end
+def show_correct
+  @word.chars.map { |letter| @correct_letters.include?(letter) ? letter : "_ "   }.join
+end
 
-  def load_game
-    if File.exist?("save.txt")
-      game = YAML::load(File.read("save.txt"))
-      puts "Game Loaded!\n"
-      game.play
-    else
-      puts "\nYou have no saved games."
-    end
-    exit
-  end
 
-  def get_random_word
-    rand_word = File.readlines('5desk.txt').select  do |w|
-      w.chomp.length > 4 && w.chomp.length < 13
-    end
-    rand_word.sample.chomp.downcase
-  end
+def show_misses
+  misses_string = ""
+  @misses.each { |miss| misses_string += "<strike>#{miss}</strike> " }
+  misses_string
+end
 
-  def wrong_guesses
-    @wrong_guesses.join(', ')
-  end
-
-  def secret_display
-    @secret_word.chars.map do |l|
-      @correct_guesses.include?(l) ? l : '_ '
-    end.join(' ')
-  end
-
-  def game_over?
-    @lost || @won
-  end
-
-  def draw
-    @guillotine += @parts.pop
-    @lost = @parts.empty?
-  end
-
-  def play
-    until game_over?
-      puts @guillotine
-      puts "Secret word: #{secret_display}"
-      puts "Misses: #{wrong_guesses}"
-      print 'Enter your guess (or type "save" to save this game): '
-      guess = gets.chomp.downcase
-      puts 'Z' * 80
-
-      if
-        guess == 'save'
-        save_game
-      elsif
-        @secret_word.include?(guess)
-        @correct_guesses << guess
-        if
-          @correct_guesses.size == Set.new(@secret_word.chars).size
-          @won = true
-        end
-      else
-        @wrong_guesses << guess unless @wrong_guesses.include?(guess)
-        draw
-      end
-    end
-
-    puts 'W' * 80
-    puts @guillotine
-    puts @won ? "You won!" : "You lost!"
-    puts "Secret word is: #{@secret_word}"
-  end
 end
